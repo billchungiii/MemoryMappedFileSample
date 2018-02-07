439b34549d10132d42ef85a2d25e03a1830094c9
billchungiii <billchungiii@gmail.com>
2018-02-07 20:12:16 +0800

    加入專案檔案。

---
+++ MemoryMappedFileSample/FileReader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Text;
+
+namespace MemoryMappedFileSample
+{
+    class FileReader
+    {
+        public static byte[] Read(string path)
+        {
+            byte[] result;
+            using (var memoryMappedFile = MemoryMappedFile.CreateFromFile(path, FileMode.Open))
+            {
+                using (var memoryMappedViewStream = memoryMappedFile.CreateViewStream())
+                {
+                    result = new byte[memoryMappedViewStream.Length];
+                    memoryMappedViewStream.Read(result, 0, result.Length);
+                }
+            }
+            return result;
+        }
+
+        public static void Write(string path,byte[] content)
+        {
+            using (var memoryMappedFile = MemoryMappedFile.CreateFromFile(path, FileMode.Create, "save", content.Length))
+            {
+                using (var memoryMappedViewStream = memoryMappedFile.CreateViewStream())
+                {
+                    memoryMappedViewStream.Write(content, 0, content.Length);
+                }
+            }
+        }
+    }
+}
---
+++ MemoryMappedFileSample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MemoryMappedFileSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var bytes = FileReader.Read("MilkyWay.jpg");
+
+            FileReader.Write("MilkyWay002.jpg",bytes );
+        }
+    }
+}
